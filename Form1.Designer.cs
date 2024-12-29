cda7d3010219716b235a00e61b113afe234113bb
wasabina67 <wasabina67@gmail.com>
2024-12-30 08:56:41 +0900

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelCounter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // labelCounter
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 97);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "0";
+            this.labelCounter.AutoSize = true;
+            this.labelCounter.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelCounter.Location = new System.Drawing.Point(0, 0);
+            this.labelCounter.Name = "labelCounter";
+            this.labelCounter.Size = new System.Drawing.Size(91, 97);
+            this.labelCounter.TabIndex = 0;
+            this.labelCounter.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(473, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelCounter);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,7 +56,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCounter;
     }
 }
 
