448ae0b9115e64543a8648e311b15d798a9a0014
wasabina67 <wasabina67@gmail.com>
2024-12-30 09:09:10 +0900

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -68,7 +68,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.ClientSize = new System.Drawing.Size(284, 211);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.labelCounter);
